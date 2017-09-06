be466b73448175d9a91cd543a0dd2e8ad87644d5
Palle Cogburn <palle@4thex.com>
2017-09-06 12:29:31 +0000

    Render spaceship

--- lesson-1/main.js
+++ lesson-1/main.js
@@ -1,14 +1,27 @@
 (function() {
    window.addEventListener("load", function(event) {
-       canvas.initialize();
+       new Canvas();
    });
    
-   var canvas = {
-     ctx: null,  
-     initialize: function() {
-         var element = document.getElementById("canvas");
-         this.ctx = element.getContext("2d");
-         this.ctx.fillRect(25, 25, 100, 100);
-     }  
+   var Canvas = function() {
+     var ctx;
+     var ship;
+     var canvas = document.getElementById("canvas");
+     ctx = canvas.getContext("2d");
+     ctx.fillStyle = "white";
+     ctx.strokeStyle = "white";
+     ctx.lineWidth = 3;
+     ctx.translate(canvas.width/2, canvas.height/2);
+     ship = new Ship(ctx);
+   };
+   
+   var Ship = function(ctx) {
+     ctx.beginPath();
+     ctx.moveTo(20, 0);
+     ctx.lineTo(-20, -10);
+     ctx.moveTo(20, 0);
+     ctx.lineTo(-20, 10);
+     ctx.closePath();
+     ctx.stroke();
    };
 }())
